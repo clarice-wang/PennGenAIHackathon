48d6b8b9a69abb512e715eb0426f9c5c8e4829a8
clarice_wang <claricewang@gmail.com>
2024-02-17 10:00:58 -0500

    redid layout

--- main.py
+++ main.py
@@ -30,31 +30,6 @@ template = """
     YOUR RESPONSE:
 """
 
-# hysterical version
-# template = """
-#     Below is a historical event you must tell a story about.
-
-#     Your goal is to:
-#     - Write a story about this historical event.
-#     - If a moment was specified, the story should focus on that particular moment.
-#     - This story should be narrated by the given character.
-#     - This story should be factually correct.
-#     - Use a storyteller tone as if you were writing a children's book.
-#     - Separate the story into five main segments, clearly marked by their segment number.
-
-#     For example:
-#     - The event is WWI, the main character is Scooby-Doo, and the specific moment is 'How It Started'
-#     - The story should be about what events led up to WWI.
-#     - The story should be told in the voice of Scooby-Doo, including his catchphrase "ruh-roh" after unfortunate events.
-    
-#     Below is the historical event, main character, and specific moment in the event:
-#     EVENT: {event}
-#     CHARACTER: {char}
-#     MOMENT: {moment}
-
-#     YOUR RESPONSE:
-# """
-
 prompt = PromptTemplate(
     input_variables=['event','char'],
     template=template,
@@ -67,62 +42,51 @@ def load_LLM():
 
 llm = load_LLM()
 
+##### PAGE LAYOUT BEGINS #####
+
 st.set_page_config(page_title="Storybook AI", page_icon=":book:")
-st.header("Storybook AI")
+st.header("📖 Storybook AI")
+
+event_text = st.text_area(label="", placeholder="Enter a historical event...", key="event_input")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("## header")
-    st.write("col1")
+    # st.markdown("## header")
+
+    option_moment = st.selectbox(
+        'Is there any specific moment the story should focus on?',
+        ('How It Started', 'How It Ended', 'Random Moment', 'Other...')
+    )
+    # make optional if user response was alr specific
+
+    if option_moment == "Other...": 
+        option_moment = st.text_input("Enter your other option...")
+
+with col2:
     option_audience = st.selectbox(
         'What age group is your audience in?',
         ('Kinder (3-6 years old)', 'Elementary (6-9 years old)', 'Middle (10+ years old)')
     )
-
-    option_moment = st.selectbox(
-        'What part of this event would you like the story to focus on?',
-        ('How It Started', 'How It Ended', 'Random Moment')
-    )
-    # make optional if user response was alr specific
-
-with col2:
-    st.write("col2")
-    st.markdown("**bold**")
-    st.image(image='andreasson2.jpeg', width=500, caption='https://saraandreasson.com/')
-
-
-# st.write(1+2)
+    # st.markdown("**bold**")
+    # st.image(image='andreasson2.jpeg', width=500, caption='https://saraandreasson.com/')
     
-event_text = st.text_area(label="", placeholder="Enter a historical event...", key="event_input")
 # char_text = st.text_area(label="", placeholder="Enter a main character...", key="char_input")
 
 # def get_text():
 #     input_text = st.text_area(label="", placeholder="Your Email...", key="email_input")
 #     return input_text
 
-if event_text:
-    # st.write("Let's learn more about ", event_text, "!")
-
-    # prompt_with_event = prompt.format(event=event_text, char=char_text, moment=option_moment)
-    prompt_with_event = prompt.format(event=event_text, moment=option_moment, audience=option_audience)
+if st.button('Generate!'):
+    prompt_with_input = prompt.format(event=event_text, moment=option_moment, audience=option_audience)
     
-    story = llm(prompt_with_event)
+    # story = llm(prompt_with_input)
+    story = "temp story"
 
-    st.write(story)
+    col1, col2 = st.columns(2)
 
-# if char_text:
-    # st.write("Featuring ", char_text, ":)")
+    with col1:
+        st.write(story)
 
-# if __name__ == '__main__':
-#     import openai
-#     client = openai.OpenAI()
-#     completion = client.chat.completions.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#         {"role": "system", "content": "You are a helpful assistant."},
-#         {"role": "user", "content": "Hello!"}
-#     ]
-#     )
-
-#     print(completion.choices[0].message)
+    with col2:
+        st.write('images go here')
